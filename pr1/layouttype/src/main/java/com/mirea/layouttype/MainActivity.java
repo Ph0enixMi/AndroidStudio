860e10414d10a3382f9f9535d76f8cd257b13c50
Ph0enixMi <100961413+Ph0enixMi@users.noreply.github.com>
2022-05-07 21:20:30 +0300

    Lesson1

---
+++ pr1/layouttype/src/main/java/com/mirea/layouttype/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mirea.layouttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
